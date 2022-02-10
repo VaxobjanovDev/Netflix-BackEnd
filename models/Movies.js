b60d13635d6fc278da5b4f5aa0db16eed868c2ef
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-02-10 10:58:02 +0500

    Added Movies mongoose model

---
+++ models/Movies.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const MoviesSchema = new mongoose.Schema(
+	{
+		title: { type: String, required: true, unique: true },
+		desc: { type: String },
+		img: { type: String },
+		imgTitle: { type: String },
+		imgSm: { type: String },
+		trailer: { type: String },
+		video: { type: String },
+		year: { type: String },
+		limit={ type: Number },
+		genre: { type: String },
+		isSeries: { type: String },
+	},
+	{
+		timestamps: true
+	}
+)
+
+module.exports = mongoose.model("Movies", MoviesSchema)
