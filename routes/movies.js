8db299aee2279335c1e7d47688d5c2378d3dd42b
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-02-11 20:58:18 +0500

    Added post,put,delete and get method

---
+++ routes/movies.js
@@ -0,0 +1,102 @@
+const router = require("express").Router();
+const Movies = require("../models/Movies");
+const verify = require("../verifyJWToken");
+
+// Post
+
+router.post("/movies", async (req, res) => {
+  if (req.user.admin) {
+    const newMovie = await Movies(req.body);
+    try {
+      const savedMovie = await newMovie.save();
+      res.status(201).json(savedMovie);
+    } catch {
+      res.status(500).json("Server error");
+    }
+  } else {
+    res.status(403).json("You are not allowed");
+  }
+});
+
+// Update
+router.put("/:id", async (req, res) => {
+  if (req.user.admin) {
+    try {
+      const updatedMovie = await Movies.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(savedMovie);
+    } catch {
+      res.status(500).json("Server error");
+    }
+  } else {
+    res.status(403).json("You are not allowed");
+  }
+});
+
+// Delete
+router.delete("/:id", async (req, res) => {
+  if (req.user.admin) {
+    try {
+      await Movies.findByIdAndDelete(req.params.id);
+      res.status(200).json("Movie has been deleted");
+    } catch {
+      res.status(500).json("Server error");
+    }
+  } else {
+    res.status(403).json("You are not allowed");
+  }
+});
+
+// Get
+router.get("/:id", verify, async (req, res) => {
+  try {
+    const movie = await Movies.findById(req.params.id);
+    res.status(200).json(movie);
+  } catch {
+    res.status(500).json("Server error");
+  }
+});
+
+// Get Random
+router.get("/random", verify, async (req, res) => {
+  const type = req.query.type;
+  let movie;
+  try {
+    if (type === "series") {
+      movie = await Movies.aggregate([
+        { $match: { isSeries: true } },
+        { $sample: { size: 1 } },
+      ]);
+    } else {
+      movie = await Movies.aggregate([
+        { $match: { isSeries: false } },
+        { $sample: { size: 1 } },
+      ]);
+    }
+    res.status(200).json(movie);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get All movies
+
+router.get("/", verify, async (req, res) => {
+  if (req.user.admin) {
+    try {
+      const movie = await Movies.find();
+      res.status(200).json(movie);
+    } catch {
+      res.status(500).json("Server error");
+    }
+  } else {
+    res.status(403).json("You are not allowed");
+  }
+});
+
+module.exports = router;
